bcca7709bb02a3ba25aadc015bb2b7703a279392
Student <muwalaka@ualberta.ca>
2021-01-28 23:19:39 -0500

    first commit

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 #  coding: utf-8 
 import socketserver
+import os
+import mimetypes
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -26,13 +28,153 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+class HTTPRequest:
+    """
+    parse HTTP request 
+    Input: data
+    """
+    def __init__(self, data):
+        self.request_method = None
+        self.request_uri = None
+        self.http_version = '1.1'
+
+        #parse the request data
+        self.parse_request(data)
+
+    #Parse HTTP Request
+    def parse_request(self, data):
+        http_request = data.split(b"\r\n")
+        
+        #for loop to print and decode the client request on the server   
+        for i in range(len(http_request)):
+            http_request[i] = http_request[i].decode("utf-8")
+            print(http_request[i])
+
+        request_line = http_request[0]
+        
+        #split self.request_line by space
+        request_line = request_line.split(" ")
+        
+        self.request_method = request_line[0]
+
+        #check if request_line contains a uri and http_version
+        if len(request_line) > 1:
+            self.request_uri = request_line[1]
+
+        if len(request_line) > 2:
+            self.http_version = request_line[2]
+
+        
+
+
 
 class MyWebServer(socketserver.BaseRequestHandler):
+
+    status_codes = {
+        200: "OK",
+        404: "Not Found",
+        501: "Not Implemented",
+    }
+
+    headers = {
+        "Connection: close",
+    }
+
+   
+
+    def status_line(self, status_code):
+        reason = self.status_codes[status_code]
+        status_line = 'HTTP/1.1 %s %s\r\n' % (status_code, reason)
+
+        # convert str to bytes
+        return status_line.encode()
+
+    def response_headers(self, extra_headers=None):
+        """Returns headers (as bytes).
+        The `extra_headers` can be a dict for sending 
+        extra headers with the current response
+        """
+        headers_copy = self.headers.copy() # make a local copy of headers
+
+        if extra_headers:
+            headers_copy.update(extra_headers)
+
+        headers = ''
+
+        for h in headers_copy:
+            headers += '%s: %s\r\n' % (h, headers_copy[h])
+
+        return headers.encode() # convert str to bytes
+
+    
+
+    def handle_GET(self, request):
+
+        path = request.request_uri.strip('/') # remove slash from URI
+
+        if not path:
+            # If path is empty, that means user is at the homepage
+            # so just serve index.html
+            status_line = self.status_line(200)
+
+            response = status_line + b"Connection: close"
+
+            return response
+
+        
+
+        if os.path.exists(path) and not os.path.isdir(path): # don't serve directories
+            status_line = self.status_line(200)
+
+        #     # find out a file's MIME type
+        #     # if nothing is found, just send `text/html`
+            content_type = mimetypes.guess_type(path)[0] 
+
+            extra_headers = {'Content-Type': content_type}
+            response_headers = self.response_headers(extra_headers)
+
+            with open(path, 'rb') as f:
+                response_body = f.read()
+        else:
+            status_line = self.status_line(404)
+            response_headers = self.response_headers()
+            response_body = b'<h1>404 Not Found</h1>'
+
+        blank_line = b'\r\n'
+
+        response = b''.join([status_line, response_headers, blank_line, response_body])
+
+        return response
+
+      
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        #store the parsed HTTP request in a variable
+        request = HTTPRequest(self.data)
+
+
+        try:
+            #construct the handle method for the request method
+            handler = getattr(self, "handle_%s" % request.request_method)
+        except:
+           # handler = self.HTTP_501_handler
+           pass
+
+
+
+        response = handler(request)
+        self.request.send(response)
+
+      
+        # print(self.request_method)
+        #print ("Got a request of: %s\n" % response)
+        #self.request.sendall(bytearray("OK",'utf-8'))
+
+   
+        
+
+  
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -43,4 +185,4 @@ if __name__ == "__main__":
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
-    server.serve_forever()
+server.serve_forever()
